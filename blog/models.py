098c5716a4d0fc32e5510a1a4812b11896c1eaa1
Tawkir Talukder <ubiquitouss@yahoo.com>
2019-10-17 08:47:23 +0600

    job app is organaized and working fully

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class blog(models.Model):
+    title = models.CharField(max_length=255, default='your title')
+    pub_date = models.DateTimeField()
+    body = models.TextField(max_length=255, default='SAY SOMETHIG')
+    image = models.ImageField(upload_to="images/")
